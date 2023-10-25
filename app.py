f18901a3d2ad18d08c1fc79e6ec65075e9023d0f
Man0dvi <mandvishukla20@gmail.com>
2023-10-25 15:53:13 +0530

    Added mysql-installer to .gitignore

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import mysql.connector
 import subprocess  # For triggering face recognition
-import os
-# from flask_sse import sse
-
-
-# os.chdir('/')
 
 app = Flask(__name__)
 
@@ -18,13 +13,9 @@ db_connection = mysql.connector.connect(
 )
 cursor = db_connection.cursor()
 
-# Counter for captured images
-captured_image_count = 0
-
 # Route for registering a student
 @app.route('/', methods=['GET', 'POST'])
 def register_student():
-    global captured_image_count  # Use the global variable for image count
 
     if request.method == 'POST':
         # Get data from the form
@@ -40,24 +31,7 @@ def register_student():
 
     return render_template('register.html')
 
-# Add a new route for processing the captured image
-# @app.route('/process_image', methods=['POST'])
-# def process_image():
-#     global captured_image_count  # Use the global variable for image count
-#     image_data = request.json.get('image')
-#
-#     # Process the image data as needed (e.g., face recognition)
-#     # You can add your face recognition logic here
-#
-#     # Example: increment the image count and trigger face recognition after 20 images
-#     captured_image_count += 1
-#     if captured_image_count >= 20:
-#         # Reset the image count for the next student
-#         captured_image_count = 0
-#         # Trigger face recognition using subprocess
-#         subprocess.run(["python", "face_recognition.py"])  # Adjust the command as needed
-#
-#     return jsonify({'result': 'Image processed successfully'})
+
 @app.route('/run_training', methods=['POST'])
 def run_training():
     # Define the path to your Python executable within the virtual environment
@@ -100,6 +74,34 @@ def finish():
         # If there's an error running the script, print the error message
         print(e.stderr)
         return "Error executing the training script."
+
+
+@app.route('/teacher')
+def teacher_interface():
+    return render_template('teacher.html')
+
+# Route for marking attendance
+@app.route('/mark_attendance', methods=['POST'])
+def mark_attendance():
+    # Define the path to your Python executable within the virtual environment
+    python_path = r"C:\Users\mandv\PycharmProjects\AutomatedAttendace\ProcterAI\venv\Scripts\python.exe"
+
+    # Define the path to your training_data.py script
+    script_path = r"C:\Users\mandv\PycharmProjects\AutomatedAttendace\ProcterAI\real_time.py"
+
+    try:
+        # Run the training_data.py script using the specified Python executable
+        result = subprocess.run([python_path, script_path], capture_output=True, text=True, check=True)
+
+        # Print the script's output
+        print(result.stdout)
+        return "Real_time script executed successfully."
+    except subprocess.CalledProcessError as e:
+        # If there's an error running the script, print the error message
+        print(e.stderr)
+        return "Error executing the real_time script."
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
